5cfb379b9903560f58cd10f8f4afc4a9e3f81bdc
Jalamang Janka <42457441+Jalamang@users.noreply.github.com>
2023-02-27 21:13:38 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld { 
 	public static void main(String[] args){
-		System.out.print("Hello Build Sonar-Jenkins-Integration --");
+		System.out.print("Hello Build Sonar-Jenkins-Integration ");
 	}
 }
